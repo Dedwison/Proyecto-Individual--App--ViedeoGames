46e3589be6c6a045c7d6a949d9611ffd10c16409
Dedwison <dedwison@gmail.com>
2022-12-16 15:20:23 -0300

    again

--- api/src/app.js
+++ api/src/app.js
@@ -17,7 +17,7 @@ server.use(morgan("dev"));
 server.use((req, res, next) => {
   res.header(
     "Access-Control-Allow-Origin",
-    "http://app-viedeogames.vercel.app"
+    "https://app-viedeogames.vercel.app"
   ); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
